05a6ca7971b39946bbd09d450ffde4e9f39b7f9c
Chris Smith <chris.smith@ahead.com>
2024-12-02 15:17:51 -0500

    solve d2p2

--- 2/Program.cs
+++ 2/Program.cs
@@ -4,45 +4,84 @@ using System.Formats.Asn1;
 using System.Text;
 using System.Transactions;
 
-Console.WriteLine("Program One");
-
-
-using StreamReader reader = new("input1.txt");
-
-var ans = 0;
-string? text = reader.ReadLine();
-while (text != null)
+class Program
 {
-    var list = text.Split(" ").Select(int.Parse).ToList();
-    var safe = true;
-    var asc = list[0] - list[1] < 0;
-    for (int i = 1; i < list.Count(); i++)
+    static void Main(string[] args)
     {
-        var movement = list[i - 1] - list[i];
-        if (movement == 0)
+        Console.WriteLine("Program One");
+
+
+        using StreamReader reader = new("input1.txt");
+
+        var ans = 0;
+        string? text = reader.ReadLine();
+        while (text != null)
         {
-            safe = false;
+            var list = text.Split(" ").Select(int.Parse).ToList();
+            var safe = checkList(list);
+            if (safe)
+            {
+                ans++;
+            }
+            else
+            {
+                // Do some fun, basically the same thing again!
+
+                for (int i = 0; i < list.Count(); i++)
+                {
+                    int val = list[i];
+                    list.RemoveAt(i);
+                    safe = checkList(list);
+                    if (safe) break;
+                    list.Insert(i, val);
+                }
+
+                if (safe)
+                {
+                    ans++;
+                }
+
+            }
+
+            text = reader.ReadLine();
+
         }
-        if (asc)
+
+        Console.WriteLine(ans);
+
+    }
+    public static bool checkList(List<int> list)
+    {
+        var safe = true;
+        var asc = list[0] - list[1] < 0;
+        for (int i = 1; i < list.Count(); i++)
         {
-            if (movement > -1 | movement < -3)
+            var movement = list[i - 1] - list[i];
+            if (movement == 0)
             {
                 safe = false;
             }
-        }
-        if (!asc)
-        {
-            if (movement < 1 | movement > 3)
+            if (asc)
             {
-                safe = false;
+                if (movement > -1 | movement < -3)
+                {
+                    safe = false;
+                }
             }
+            if (!asc)
+            {
+                if (movement < 1 | movement > 3)
+                {
+                    safe = false;
+                }
+            }
+
         }
+        return safe;
+
     }
-    if (safe)
-    {
-        ans++;
-    }
-    text = reader.ReadLine();
 
 }
-Console.WriteLine(ans);
+
+
+
