3b833a9c37a972feec127616675d84b587b31510
Chris Smith <wingedkraby@gmail.com>
2024-12-08 20:00:48 -0500

    solve D6P2. Don't code while tired kids

--- 6/Program.cs
+++ 6/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Data;
 using System.Drawing;
+using System.Net;
 using System.Runtime.ExceptionServices;
 using System.Runtime.InteropServices;
 
@@ -9,6 +10,8 @@ Console.WriteLine("Program One");
 using StreamReader reader = new("input.txt");
 var visited = new HashSet<(int, int)>();
 (int, int) pos = (0, 0);
+(int, int) start_pos = (0, 0);
+
 List<List<char>> map = new List<List<char>>();
 var dir = 0; // 0=up  1=right 2=down 3=left
 
@@ -20,20 +23,17 @@ while (text != null)
     if (text.Contains("^"))
     {
         pos = (text.IndexOf("^"), map.Count() - 1);
+        start_pos = (text.IndexOf("^"), map.Count() - 1);
     }
     text = reader.ReadLine();
 }
-
-Console.WriteLine(pos);
 
 //Account for being here
 visited.Add(pos);
 
 //Walk the road. The witches road, while in the map!
-Console.WriteLine(map[0].Count);
 while (pos.Item1 >= 0 && pos.Item1 < map[0].Count && pos.Item2 >= 0 && pos.Item2 < map.Count)
 {
-    Console.WriteLine("IN LOOP");
     visited.Add(pos);
 
     //Check in front
@@ -111,6 +111,112 @@ while (pos.Item1 >= 0 && pos.Item1 < map[0].Count && pos.Item2 >= 0 && pos.Item2
     }
 }
 
-Console.WriteLine(pos);
+//Using list of squares visited, lets create a map..diffrent each time
 
-Console.WriteLine(visited.Count());
+
+var ans = 0;
+visited
+    .ToList()
+    .ForEach(v =>
+    {
+        var looped = new HashSet<(int, int, int)>();
+        pos = start_pos;
+        var orig = map[v.Item2][v.Item1];
+        if (orig == '.')
+        {
+            map[v.Item2][v.Item1] = '#';
+            dir = 0;
+
+            while (
+                pos.Item1 >= 0
+                && pos.Item1 < map[0].Count
+                && pos.Item2 >= 0
+                && pos.Item2 < map.Count
+            )
+            {
+                if (!looped.Add((pos.Item1, pos.Item2, dir)))
+                {
+                    Console.WriteLine("WE LOOPED BITCH");
+                    ans++;
+                    break;
+                }
+
+                //Check in front
+                if (dir == 0)
+                {
+                    try
+                    {
+                        if (map[pos.Item2 - 1][pos.Item1] == '#')
+                        {
+                            dir = 1; //turn
+                        }
+                        else
+                        {
+                            pos = (pos.Item1, pos.Item2 - 1);
+                        }
+                    }
+                    catch
+                    {
+                        pos = (pos.Item1, pos.Item2 - 1);
+                    }
+                }
+                else if (dir == 1)
+                {
+                    try
+                    {
+                        if (map[pos.Item2][pos.Item1 + 1] == '#')
+                        {
+                            dir = 2; //turn
+                        }
+                        else
+                        {
+                            pos = (pos.Item1 + 1, pos.Item2);
+                        }
+                    }
+                    catch
+                    {
+                        pos = (pos.Item1 + 1, pos.Item2);
+                    }
+                }
+                else if (dir == 2)
+                {
+                    try
+                    {
+                        if (map[pos.Item2 + 1][pos.Item1] == '#')
+                        {
+                            dir = 3; //turn
+                        }
+                        else
+                        {
+                            pos = (pos.Item1, pos.Item2 + 1);
+                        }
+                    }
+                    catch
+                    {
+                        pos = (pos.Item1, pos.Item2 + 1);
+                    }
+                }
+                else if (dir == 3)
+                {
+                    try
+                    {
+                        if (map[pos.Item2][pos.Item1 - 1] == '#')
+                        {
+                            dir = 0; //turn
+                        }
+                        else
+                        {
+                            pos = (pos.Item1 - 1, pos.Item2);
+                        }
+                    }
+                    catch
+                    {
+                        pos = (pos.Item1 - 1, pos.Item2);
+                    }
+                }
+            }
+            map[v.Item2][v.Item1] = orig;
+        }
+    });
+
+Console.WriteLine(ans);
