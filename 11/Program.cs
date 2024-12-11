d6d21fb2355f6c85e3a1a8a62a33ee80d1741ddd
Chris Smith <chris.smith@ahead.com>
2024-12-11 10:58:03 -0500

    faster, but I need more speed

--- 11/Program.cs
+++ 11/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Buffers;
 using System.Globalization;
+using Microsoft.VisualBasic;
 
 Console.WriteLine("Day 11");
 
@@ -14,52 +15,62 @@ Dictionary<string, string> calcs = new Dictionary<string, string>();
 Dictionary<string, (string, string)> splits = new Dictionary<string, (string, string)>();
 Dictionary<int, bool> even = new Dictionary<int, bool>();
 
-Stack<(long, int)> rocks = new Stack<(long, int)>();
+Stack<(string, int)> rocks = new Stack<(string, int)>(2038686383);
 
 int ans = 0;
-
-
-for (int x = 0; x < 75; x++)
+//Add initial to stack
+foreach (string s in nums)
 {
-    for (int i = 0; i < nums.Count(); i++)
-    {
-        if (nums[i] == "0")
-        {
-            nums[i] = "1";
-            continue;
-        }
-        if (!even.ContainsKey(nums[i].Length))
-        {
-            even[nums[i].Length] = nums[i].Length % 2 == 0;
-        }
-        if (even[nums[i].Length])
-        {
-
-            if (!splits.ContainsKey(nums[i]))
-            {
-                var toSplit = nums[i];
-                splits[nums[i]] = (long.Parse(toSplit.Substring(0, toSplit.Length / 2)).ToString(), long.Parse(toSplit.Substring(toSplit.Length / 2)).ToString());
-
-            }
-            var real = splits[nums[i]];
-            nums[i] = real.Item2;
-            nums.Add(real.Item1);
-            i++;
-        }
-        else
-        {
-            if (!calcs.ContainsKey(nums[i]))
-            {
-                calcs[nums[i]] = (long.Parse(nums[i]) * 2024).ToString();
-            }
-            nums[i] = calcs[nums[i]];
-        }
-
-    }
-    Console.WriteLine(x);
-    Console.WriteLine(nums.Count);
+    rocks.Push((s, 0));
 }
+
+
+while (rocks.Count != 0)
+{
+
+    var r = rocks.Pop();
+
+    if (r.Item2 == 50)
+    {
+        // Console.Write(r.Item1 + " ");
+        ans++;
+        continue;
+    }
+
+    if (r.Item1 == "0")
+    {
+        rocks.Push(("1", r.Item2 + 1));
+        continue;
+    }
+    if (!even.ContainsKey(r.Item1.Length))
+    {
+        even[r.Item1.Length] = r.Item1.Length % 2 == 0;
+    }
+    if (even[r.Item1.Length])
+    {
+        //Console.WriteLine("splitting: " + r);
+        if (!splits.ContainsKey(r.Item1))
+        {
+            splits[r.Item1] = (long.Parse(r.Item1.Substring(0, r.Item1.Length / 2)).ToString(), long.Parse(r.Item1.Substring(r.Item1.Length / 2)).ToString());
+
+        }
+        var real = splits[r.Item1];
+        rocks.Push((real.Item2, r.Item2 + 1));
+        rocks.Push((real.Item1, r.Item2 + 1));
+    }
+    else
+    {
+        if (!calcs.ContainsKey(r.Item1))
+        {
+            calcs[r.Item1] = (long.Parse(r.Item1) * 2024).ToString();
+        }
+        rocks.Push((calcs[r.Item1], r.Item2 + 1));
+    }
+}
+
 //nums.ToList().ForEach(n => Console.WriteLine(n));
 
 
-Console.WriteLine(nums.Count);
+Console.WriteLine();
+
+Console.WriteLine(ans);
